6c9f200d4d01a14d00ac70872e524adbb8bd226e
Tsai-Chieh <d0177381@gmail.com>
2022-06-25 20:33:10 +0800

    :art: format

--- javascript/1351-count-negative-numbers-in-a-sorted-matrix.js
+++ javascript/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -1,20 +1,21 @@
 /**
- * @param {number[][]} grid
+ * @param {Array<Array<number>>} grid
  * @return {number}
  */
-
 const countNegatives = function(grid) {
     const gridFlat = grid.flat()
     let count = 0
-    gridFlat.forEach(num => { if (num < 0) count++ })
+    gridFlat.forEach((num) => {
+        if (num < 0) count++
+    })
     return count
-};
+}
 
 const grid = [
     [4, 3, 2, -1],
     [3, 2, 1, -1],
     [1, 1, -1, -2],
     [-1, -1, -2, -3],
-];
+]
 
 console.log(countNegatives(grid))
