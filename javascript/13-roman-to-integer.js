e8dce2303d82c69ca8c30e884244cc729bb37c09
Tsai-Chieh <d0177381@gmail.com>
2022-10-22 21:02:29 +0800

    :sparkles: add new func with new Map method

--- javascript/13-roman-to-integer.js
+++ javascript/13-roman-to-integer.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-const romanToInt = function(s) {
+const romanToInt0 = function(s) {
   const map = {
     'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
   }
@@ -13,6 +13,30 @@ const romanToInt = function(s) {
   }
   return sum
 }
-console.log(romanToInt('III')) // 3
-console.log(romanToInt('LVIII')) // 58
-console.log(romanToInt('MCMXCIV')) // 58
+console.log(romanToInt0('III')) // 3
+console.log(romanToInt0('LVIII')) // 58
+console.log(romanToInt0('MCMXCIV')) // 1994
+
+const romanToInt1 = function(s) {
+  const map = new Map([
+    ['I', 1],
+    ['V', 5],
+    ['X', 10],
+    ['L', 50],
+    ['C', 100],
+    ['D', 500],
+    ['M', 1000]],
+  )
+  let sum = 0
+  for (let i = 0; i < s.length; i++) {
+    const curr = s[i]
+    const next = s[i + 1]
+
+    if (map.get(next) > map.get(curr)) sum -= map.get(curr)
+    else sum += map.get(curr)
+  }
+  return sum
+}
+console.log(romanToInt1('III')) // 3
+console.log(romanToInt1('LVIII')) // 58
+console.log(romanToInt1('MCMXCIV')) // 58
