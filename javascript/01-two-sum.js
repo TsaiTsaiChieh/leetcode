8a122aed07d60678b35311117bceb8081c1e5e9d
TsaiTsaiChieh <d0177381@gmail.com>
2026-03-01 10:50:56 +0800

    ♻️ add use Map method

--- javascript/01-two-sum.js
+++ javascript/01-two-sum.js
@@ -32,3 +32,19 @@ const twoSum1 = function(nums, target) {
 console.log(twoSum1([2, 7, 11, 15], 9)) // [0, 1]
 console.log(twoSum1([3, 2, 4], 6)) // [1, 2]
 console.log(twoSum1([3, 3], 6)) // [0, 1]
+
+const twoSum2 = function(nums, target) {
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i]
+    const partner = target - nums[i];
+    if (map.has(partner)) {
+      return [map.get(partner), i]
+    }
+    map.set(currentNum, i)
+  }
+}
+
+console.log(twoSum2([2, 7, 11, 15], 9)) // [0, 1]
+console.log(twoSum2([3, 2, 4], 6)) // [1, 2]
+console.log(twoSum2([3, 3], 6)) // [0, 1]
