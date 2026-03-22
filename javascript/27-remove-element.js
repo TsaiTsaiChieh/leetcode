150f2ea0581f4a81410330c56856615e94071f02
TsaiTsaiChieh <d0177381@gmail.com>
2026-03-22 14:51:09 +0800

    fix: improve removeElement function for better readability and structure

--- javascript/27-remove-element.js
+++ javascript/27-remove-element.js
@@ -4,15 +4,17 @@
  * @return {number}
  */
 var removeElement1 = function (nums, val) {
-    let k = 0
-    for (let i = 0; i < nums.length; i++) {
-        const currentNum = nums[i];
-        if (currentNum !== val) {
-            nums[k] = currentNum;
-            k++;
-        }
+  if (nums.length === 0) return 0;
+
+  let k = 0; // k is the position to place the next valid number
+  for (let i = 0; i < nums.length; i++) {
+    const current = nums[i];
+    if (current !== val) {
+      nums[k] = current; // put valid number
+      k++;
     }
-    return k;
+  }
+  return k;
 };
-console.log(removeElement1([3, 2, 2, 3], 3)) // 2
-console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)) // 5
+console.log(removeElement1([3, 2, 2, 3], 3)); // 2
+console.log(removeElement1([0, 1, 2, 2, 3, 0, 4, 2], 2)); // 5
