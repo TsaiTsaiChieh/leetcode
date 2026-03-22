f101226ab0308448df85a2684a99b1f4474373d6
TsaiTsaiChieh <d0177381@gmail.com>
2026-03-22 16:42:26 +0800

    ✨ add Find the Index of the First Occurrence in a String solution in JavaScript

---
+++ javascript/28-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  if (haystack.length < needle.length) {
+    return -1;
+  }
+  return haystack.indexOf(needle);
+};
+
+console.log(strStr('hello', 'll')); // 2
