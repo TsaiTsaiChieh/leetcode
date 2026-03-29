c485a1bf3895c9f84d3be2c12814d0f002e71dbe
TsaiTsaiChieh <d0177381@gmail.com>
2026-03-29 22:34:57 +0800

    ✨ add Sqrt(x) solution in JavaScript

---
+++ javascript/69-sqrtx.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  return Math.floor(Math.pow(x, 0.5));
+};
+
+console.log(mySqrt(4)); // 2
+console.log(mySqrt(8)); // 2
